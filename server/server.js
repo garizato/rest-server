ab12639cad3ea549cf48301fcecbeaa88714949d
Gerardo <garizato@gmail.com>
2020-07-10 23:49:35 -0500

    2do cambio version

--- server/server.js
+++ server/server.js
@@ -2,16 +2,18 @@ require('./config/config');
 
 const express = require('express');
 const app = express();
+
 const bodyParser = require('body-parser');
 
 // parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({ extended: false }))
 
 // parse application/json
-app.use(bodyParser.json());
+app.use(bodyParser.json())
+
 
 app.get('/usuario', function(req, res) {
-    res.json('Get Usuario');
+    res.json('get Usuario LOCAL!!!');
 });
 
 app.post('/usuario', function(req, res) {
@@ -19,20 +21,22 @@ app.post('/usuario', function(req, res) {
     let body = req.body;
 
     if (body.nombre === undefined) {
+
         res.status(400).json({
             ok: false,
             mensaje: 'El nombre es necesario'
         });
+
     } else {
         res.json({
             persona: body
         });
     }
 
-
 });
 
 app.put('/usuario/:id', function(req, res) {
+
     let id = req.params.id;
 
     res.json({
@@ -45,5 +49,5 @@ app.delete('/usuario', function(req, res) {
 });
 
 app.listen(process.env.PORT, () => {
-    console.log('Escuchando el puerto: ', process.env.PORT);
+    console.log('Escuchando puerto: ', process.env.PORT);
 });
