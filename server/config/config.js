3d3375c7af7d3a2df100565499a2a50dcb8c3ebe
Gerardo <garizato@gmail.com>
2020-07-12 22:49:27 -0500

    Variable de entorno MONGO_DB

--- server/config/config.js
+++ server/config/config.js
@@ -17,7 +17,7 @@ let urlBD;
 if (process.env.NODE_ENV === 'dev') {
     urlBD = 'mongodb://localhost:27017/cafe';
 } else {
-    urlBD = 'mongodb+srv://garizato:j5kYtsLDzpu7AQhe@cluster0.mbeol.mongodb.net/cafe';
+    urlBD = process.env.MONGO_URI;
 }
 
 //urlBD = 'mongodb+srv://garizato:j5kYtsLDzpu7AQhe@cluster0.mbeol.mongodb.net/cafe';
